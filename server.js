de90164a0dd385078a8387e92e47cdc48e39f34b
Vick Mahant <viratmahant@gmail.com>
2015-04-01 17:55:34 -0400

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,10 @@ app.set('port', (process.env.PORT || 5000));
 app.use(express.static('./public'));
 
 app.get('/', function(req, res) {
-	res.sendfile('What the fuck its me');/*./public/index.html');*/
+	res.sendfile('./public/index.html');
+});
+app.get('/storytime', function(req, res) {
+	res.send("<p>Hey"+req.query.name+"Once upon a time</p>");
 });
 
 app.listen(app.get('port'), function() {
