e4db8203928f40b1a898f6a9a6bd4d54db5593a8
Vick Mahant <viratmahant@gmail.com>
2015-04-01 17:50:16 -0400

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 5000));
 app.use(express.static('./public'));
 
 app.get('/', function(req, res) {
-	res.sendfile('./public/index.html');
+	res.sendfile('What the fuck its me');/*./public/index.html');*/
 });
 
 app.listen(app.get('port'), function() {
